8e2deb4705642a2aa39a687bdd738532642a8374
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-26 21:49:38 +0530

    test for note search that returns no matches

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -24,4 +24,27 @@ RSpec.describe Note, type: :model do
     expect(Note.search("first")).to include(note1, note3)    
     expect(Note.search("first")).not_to include(note2)    
   end
+
+  it "returns an empty collection when no notes are found" do
+    user = User.create(
+      first_name: "Ravi",
+      last_name: "Ashwin",
+      email: "r@a.c",
+      password: "password"
+    )
+    project = user.projects.create(name: "Test Project")
+    note1 = project.notes.create(
+      message: "This is the first note.",
+      user: user
+    )
+    note2 = project.notes.create(
+      message: "This is the second note.",
+      user: user
+    )
+    note3 = project.notes.create(
+      message: "First, write me a lovely note.",
+      user: user
+    )
+    expect(Note.search("blah")).to be_empty
+  end
 end
