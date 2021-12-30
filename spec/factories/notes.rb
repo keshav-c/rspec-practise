aef2f88b4bc7a619238e20a3b62da4031ec9c3c3
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-30 09:40:09 +0530

    Prevent extra user creation while creating note from factory
    In notes factory, an extra user is created by project if it is
    called before the user association. Explicity link the user to
    project owner, to prevent this.

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :note do
     # Explicit or implicit assocation can be used
     # (data retrieved from the corresponding factory)
-    user
     project
+    user { project.owner }
     message { "A sample note." }
   end
 end
