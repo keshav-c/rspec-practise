eba81311d6b9ff8317bf2f4f83ecae4c734f2dd7
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-30 22:42:41 +0530

    A basic controller get test

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe "#index" do
+    it "responds successfully" do
+      get :index
+      # success has been deprecated for rails 6
+      # expect(response).to be_successful               # <- works
+      expect(response).to have_http_status :success     # <- also works
+    end
+  end
+end
