5a7b8dae8f4c819f2bb4417604308b3808ac0a63
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-30 12:01:45 +0530

    Remove duplication in projects factory, this time using traits

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -7,15 +7,15 @@ FactoryBot.define do
     # specify the factory in the explicit association below
     association :owner
 
-    factory :project_due_yesterday do
+    trait :due_yesterday do
       due_on { 1.day.ago }
     end
 
-    factory :project_due_today do
+    trait :due_today do
       due_on { Date.current.in_time_zone }
     end
 
-    factory :project_due_tomorrow do
+    trait :due_tomorrow do
       due_on { 1.day.from_now }
     end
   end
