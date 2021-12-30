2a5800c503f9d6ef15c72c0a1a0346621cf6bf48
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-30 11:58:34 +0530

    Remove duplication in projects factory using inheritance

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -6,26 +6,17 @@ FactoryBot.define do
     # Either specify the user alias (owner) in it's factory or 
     # specify the factory in the explicit association below
     association :owner
-  end
 
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Project #{n}"}
-    description { "A sample project." }
-    due_on { 1.day.ago }
-    association :owner
-  end
+    factory :project_due_yesterday do
+      due_on { 1.day.ago }
+    end
 
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Project #{n}"}
-    description { "A sample project." }
-    due_on { Date.current.in_time_zone }
-    association :owner
-  end
+    factory :project_due_today do
+      due_on { Date.current.in_time_zone }
+    end
 
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Project #{n}"}
-    description { "A sample project." }
-    due_on { 1.day.from_now }
-    association :owner
+    factory :project_due_tomorrow do
+      due_on { 1.day.from_now }
+    end
   end
 end
