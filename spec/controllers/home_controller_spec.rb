0521938f912c3260416a2ae08d1a3e027e6cf210
Keshav Chakravarthy <keshav1985@gmail.com>
2021-12-31 10:00:06 +0530

    ok also represents status 2xx

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe HomeController, type: :controller do
       get :index
       # success has been deprecated for rails 6
       # expect(response).to be_successful               # <- works
+      # expect(response).to have_http_status :ok        # <- also works
       expect(response).to have_http_status :success     # <- also works
     end
   end
